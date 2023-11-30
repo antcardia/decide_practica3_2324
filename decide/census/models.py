c8be01e48a8ce9fe88090b5a04888bb7336d4d8f
antcardia <antcardia4@alum.us.es>
2023-11-30 11:48:20 +0100

    Hola

--- decide/census/models.py
+++ decide/census/models.py
@@ -5,6 +5,7 @@ class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
     adscripcion = models.CharField(max_length=256, default='none') 
+    hola
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
