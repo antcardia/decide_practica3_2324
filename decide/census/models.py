90a4bcb6f604e83a1b00fee6bdc3dbaa042113c4
antcardia <antcardia4@alum.us.es>
2023-11-30 11:47:49 +0100

    Que tal

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
-    adscripcion = models.CharField(max_length=256, default='none') 
+    adscripcion = models.CharField(max_length=256, default='none')
+    que tal
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
